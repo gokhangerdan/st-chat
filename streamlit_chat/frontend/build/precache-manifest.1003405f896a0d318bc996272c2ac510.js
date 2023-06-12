5b7215fd13e723077b66988008b424403c6a47c6
Gökhan Gerdan <ggerdan@bimser.com.tr>
2023-06-12 15:20:47 +0300

    build successful

---
+++ streamlit_chat/frontend/build/precache-manifest.1003405f896a0d318bc996272c2ac510.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c968e3414ba29e6f55f14c5ed15b86",
+    "url": "./index.html"
+  },
+  {
+    "revision": "081a81a9c3402396c275",
+    "url": "./static/js/2.f4f2a8b1.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.f4f2a8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bfa897387d5fe03070c",
+    "url": "./static/js/main.0b3705cb.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
